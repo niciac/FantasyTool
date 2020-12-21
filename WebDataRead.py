26a4aa53187bc21fa1d9d5dcb97b8b26551d1a8e
Nicolas Iacovou <nicolas.iacovou@icloud.com>
2020-12-21 00:16:30 +0000

    WebDataRead
    Can now open an automation session in safari

--- WebDataRead.py
+++ WebDataRead.py
@@ -1,10 +1,15 @@
-import bs4
-import tifffile
-import selenium
 
-# Tool that reads data from webpages
+#Simple assignment
+from selenium.webdriver import Safari
+import time
 
-print("Hello World")
+driver = Safari()
 
-# Playing around with BeautifulSoup4
+# driver = webdriver.Safari("/Applications/Safari.app")
+# driver = webdriver.Firefox("/Applications/Firefox.app")
 
+driver.set_window_size(1000,600)
+driver.get("http://fplstatistics.com/Home/Events")
+print(driver.current_url)
+time.sleep(10)
+driver.quit()
