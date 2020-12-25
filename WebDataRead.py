bb619983d11a24af223728d2ca266aca823d9285
Nicolas Iacovou <nicolas.iacovou@icloud.com>
2020-12-25 17:40:44 +0000

    added folder that contains text files created by the code to gitignore file. Removed all text files from main folder. Now stringToTxtFile function saves everything in a file

--- WebDataRead.py
+++ WebDataRead.py
@@ -1,33 +1,39 @@
 
 # Importing Required Libraries
+import os
 import csv
 import time
 from selenium.webdriver import Safari
 from bs4 import BeautifulSoup
 
+# Function that writes input to a text file
+def stringToTxtFile(data,filename,directory):
+    dir = directory
+    if os.path.exists(dir):
+        os.chdir(dir)
+    else:
+        os.mkdir(dir)
+        os.chdir(dir)
+    print("Now saving in: ", os.getcwd())
+    datafile = open("{a}.txt".format(a=filename),'w')
+    datafile.write(data)
+    datafile.close()
+    os.chdir('..')
+    print(os.getcwd())
+
 # Setting Safari as the webdriver
 driver = Safari()
-
+# Opening safari driver and loading website
 driver.set_window_size(1000,600)
-driver.get("http://fplstatistics.com/Home/Events")
-# print(driver.current_url)
+driver.get("http://fplstatistics.com/Home/Events",)
+print(driver.current_url)
 
 # Get the webpage source code
 content = driver.page_source
-# print(content)
-
-# # Write source code into text file
-# datafile = open("data.txt",'w')
-# datafile.write(content)
-# datafile.close()
-
-# Extracting specific data from website
-soup = BeautifulSoup(content)
-# print(soup.prettify())
-# print(soup.get_text()) # returns just text but might make it more difficult to use the data
-print(soup.find_all('role="row"'))
 
 # Wait 5 seconds and close webdriver session
-time.sleep(5)
+# time.sleep(1)
 driver.quit()
 
+# Extracting specific data from website
+soup = BeautifulSoup(content,'html.parser')
