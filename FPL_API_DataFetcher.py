eddb0cd817b586c022c19389adf2ba2fb01c87de
niciac <nicolas.iacovou@icloud.com>
2022-12-03 17:36:27 +0000

    Update to FPL_API_DataFetcher.py 1. New requires_completion function that chekcs if the URL requires a completion 2. New get_urls function that fetches the URLS from the configuration file

--- FPL_API_DataFetcher.py
+++ FPL_API_DataFetcher.py
@@ -3,95 +3,82 @@
 
 from datetime import date
 import json
+import logging
 from pathlib import Path as path
 import requests
 
-class URL():
-    pass
+import config_reader
+from config_reader import get_urls, settings
 
-def requires_completion(string:str)->bool:
-    
-    '''Checks if the configuration string requires a completion 
-    from the completion section of the configuration file.'''
-    
-    for char in string:
-        if char == '{':
-            opening = True
-        elif char == '}':
-            closing = True
-        else:
-            continue
-    return opening and closing
-
-def get_urls(config_file_path:path=path('personal_config.yaml')):
-    config = ConfigParser()
-    config.read(config_file_path)
-    
-    main_endpoint_url = config.get('URL','MAIN_ENDPOINT_URL')
-    general_information_endpoint_path = config.get('URL','GENERAL_INFORMATION_ENDPOINT_PATH')
-    fixtures_url_endpoint_path = config.get('URL','FIXTURES_URL_ENDPOINT_PATH')
-    player_detailed_data_endpoint_path = config.get('URL','player_detailed_data_endpoint_path')
-    element_id = config.get('URL_COMPLETION_VALUES','ELEMENT_ID')
-
-    # requested_url = main_endpoint_url + fixtures_url_endpoint_path.format(event=1)
-    # requested_url = list(config.get('URL_COMPLETION_VALUES','list'))
-    requested_url = main_endpoint_url + general_information_endpoint_path
-    return requested_url
-
-# print(get_urls())
-
+# settings = config_reader.load_settings()
 
 # Authenticated Access
-def API_authentication(login_email:str=get_login_info_yaml()['login_email'], password:str=get_login_info_yaml()['password'], fpl_manager_id:str=fpl_manager_id):
+def API_authentication(
+    login_email: str = config_reader.get_login_info(settings)["login_email"],
+    password: str = config_reader.get_login_info(settings)["password"],
+    fpl_manager_id: str = settings["url_completion_values"][
+    "fpl_manager_id"]):
+
     session = requests.session()
-    
-    url = 'https://users.premierleague.com/accounts/login/'
+
+    url = "https://users.premierleague.com/accounts/login/"
     payload = {
-        'password': password,
-        'login': login_email,
-        'redirect_uri': 'https://fantasy.premierleague.com/a/login',
-        'app': 'plfpl-web'
+        "password": password,
+        "login": login_email,
+        "redirect_uri": "https://fantasy.premierleague.com/a/login",
+        "app": "plfpl-web",
     }
 
     session.post(url, data=payload)
 
-    response = session.get(f'https://fantasy.premierleague.com/drf/my-team/{fpl_manager_id}')
+    response = session.get(
+        f"https://fantasy.premierleague.com/drf/my-team/{fpl_manager_id}"
+    )
 
     print(type(response))
 
     return response
 
+def user_input():
+    data = input("Enter choices, separated by spaces:\n")
+    print(*data.split(" "))
+
+
+urls = get_urls(settings)
+
+
 # get API data withour authentication
-def API_request_without_authentication():
+def API_request_without_authentication(urls:dict=urls):
     # Use requests to get request from API
-    request = requests.get(get_urls())
+    url = urls['main_endpoint_url']['link'] + urls['general_information_endpoint_path']['link']
+    request = requests.get(url)
     data_nested_dict = request.json()
     return data_nested_dict
 
 # save downloaded data to json format
-def save_dated_json_file(data_nested_dict = API_request_without_authentication()):
+def save_dated_json_file(data_nested_dict:dict):
     x = date.today()
     day, month, year = x.day, x.month, x.year
-    json_output_file_path = path('Data','raw','general_information',f'{year}-{str(month).zfill(2)}-{str(day).zfill(2)}.json')
+    json_output_file_path = path(
+        "Data",
+        "raw",
+        "general_information",
+        "2022-2023",
+        f"{year}-{str(month).zfill(2)}-{str(day).zfill(2)}.json",
+    )
     print(str(json_output_file_path))
 
     if json_output_file_path.exists():
         raise IOError("File already exists")
     else:
-        output_file = open(json_output_file_path,'w')
-        json.dump(data_nested_dict,output_file, indent=4)
-        
+        output_file = open(json_output_file_path, "w")
+        json.dump(data_nested_dict, output_file, indent=4)
+
         output_file.close()
 
-def user_input():
-    data = input('Enter choices, separated by spaces:\n')
-    print(*data.split(' '))
-
-# user_input()
-# print(get_login_info_ini())
-
 if __name__ == "__main__":
-    save_dated_json_file()
+    raw_data = API_request_without_authentication()
+    save_dated_json_file(raw_data)
 
 # information about endpoints found in the below article
 # https://medium.com/@frenzelts/fantasy-premier-league-api-endpoints-a-detailed-guide-acbd5598eb19
